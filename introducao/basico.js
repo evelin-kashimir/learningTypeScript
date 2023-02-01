120a7723a58fca5829282e971d9b3915915233df
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-02-01 10:32:41 -0300

    [01] - Compilando o código de diferentes formas

---
+++ introducao/basico.js
@@ -0,0 +1,7 @@
+"use strict";
+const a = 'Teste TS (Web) v2!';
+console.log(a);
+let soma = 0;
+soma = 5 + 9;
+console.log(soma);
+console.log(soma + a);
---
+++ introducao/basico.ts
@@ -0,0 +1,9 @@
+const a: string = 'Teste TS (Web) v2!';
+console.log(a);
+
+let soma: number = 0;
+soma = 5 + 9;
+
+console.log(soma);
+
+console.log(soma + a);
