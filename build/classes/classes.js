2f239b6fa3e2a4c6b97a55b7adb0ea73e8916b4e
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-02-03 18:04:54 -0300

    [10] - Simplificando a criação da classe

--- build/classes/classes.js
+++ build/classes/classes.js
@@ -1,5 +1,6 @@
 "use strict";
 class Data {
+    //Parametros default
     constructor(dia = 1, mes = 1, ano = 1970) {
         this.dia = dia;
         this.mes = mes;
@@ -7,7 +8,17 @@ class Data {
     }
 }
 const aniversario = new Data(3, 11, 1991);
-const casamento = new Data(10, 10, 2025);
 console.log(aniversario);
+class DataSimplificada {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const casamento = new Data(10, 10, 2025);
 console.log(casamento);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -3,6 +3,7 @@ class Data {
     public mes: number;
     public ano: number;
     
+    //Parametros default
     constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
         this.dia = dia;
         this.mes = mes;
@@ -10,6 +11,17 @@ class Data {
     }
 }
 const aniversario = new Data(3, 11, 1991);
-const casamento = new Data(10, 10, 2025);
 console.log(aniversario);
+
+class DataSimplificada {
+    constructor( public dia: number = 1,
+        public mes: number = 1,
+        public ano: number = 1970){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+
+const casamento = new Data(10, 10, 2025);
 console.log(casamento);
