24e037a874f0d903f8635e4dd239c990cd2551ad
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-02-03 18:42:55 -0300

    [12] - Melhorando o codigo do desafio

--- build/classes/desafios.js
+++ build/classes/desafios.js
@@ -10,17 +10,28 @@ class Produto {
         this.nome = nome;
         this.preco = preco;
         this.desconto = desconto;
-        this.nome = nome,
-            this.preco = preco,
-            this.desconto = desconto;
     }
-    aplicarDesconto() {
+    /* Aplica diretamente o desconto
+    public aplicarDesconto(): number{
         return this.preco -= (this.desconto * this.preco);
+    } */
+    //Aplica o desconto e retorna um novo preco no resumo, não diminuindo direto da propriedade
+    precoComDesconto(preco, desconto) {
+        if (desconto != 0) {
+            return `Preço com desconto R$ ${preco - (desconto * preco)}`;
+        }
+        return '';
+    }
+    resumo() {
+        return `Produto: ${this.nome}\nPreço R$ ${this.preco}\nDesconto: %${this.desconto * 100}\n` +
+            this.precoComDesconto(this.preco, this.desconto);
     }
 }
 const celular = new Produto('Iphone 15', 5000);
 const noteBook = new Produto('Acer Aspire 10', 10000, 0.1);
-noteBook.aplicarDesconto();
+//noteBook.aplicarDesconto();
 console.log(noteBook);
+console.log(noteBook.resumo());
 console.log(celular);
+console.log(celular.resumo());
 //# sourceMappingURL=desafios.js.map
--- classes/desafios.ts
+++ classes/desafios.ts
@@ -6,20 +6,35 @@
 */
 
 class Produto {
-    constructor(public nome: string, public preco: number, public desconto: number = 0){
-        this.nome = nome,
-        this.preco = preco,
-        this.desconto = desconto
-    }
+    constructor(
+        public nome: string, 
+        public preco: number, 
+        public desconto: number = 0){ }
 
+    /* Aplica diretamente o desconto
     public aplicarDesconto(): number{
         return this.preco -= (this.desconto * this.preco);
+    } */
+
+    //Aplica o desconto e retorna um novo preco no resumo, não diminuindo direto da propriedade
+    public precoComDesconto(preco: number, desconto: number): string {
+        if(desconto != 0) {
+            return `Preço com desconto R$ ${preco - (desconto * preco)}`
+        }
+        return '';
+    }
+
+    public resumo(): string {
+        return `Produto: ${this.nome}\nPreço R$ ${this.preco}\nDesconto: %${this.desconto * 100}\n`+
+        this.precoComDesconto(this.preco, this.desconto);
     }
 }
 
 const celular = new Produto('Iphone 15', 5000);
 const noteBook = new Produto('Acer Aspire 10', 10000, 0.1);
 
-noteBook.aplicarDesconto();
+//noteBook.aplicarDesconto();
 console.log(noteBook);
+console.log(noteBook.resumo());
 console.log(celular);
+console.log(celular.resumo());
