3acc44395ed5ff393e4e06588bfb06a6980b9824
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-02-09 12:57:58 -0300

    [21] - Namespaces aninhados

--- build/namespaces/namespace.js
+++ build/namespaces/namespace.js
@@ -1,19 +1,31 @@
 "use strict";
 //Namespaces são espaços reservados para garantir que nomes iguais de váriaveis globais
 //funções e etc, sejam específicas para aquela classe, e não para os demais arquivos
-var Areas;
-(function (Areas) {
-    const PI = 3.14;
-    function circunferencia(raio) {
-        return PI * Math.pow(raio, 2);
-    }
-    Areas.circunferencia = circunferencia;
-    function retangulo(base, altura) {
-        return base * altura;
-    }
-    Areas.retangulo = retangulo;
-})(Areas || (Areas = {}));
+// namespace Areas {
+//     const PI = 3.14;
+//     export function circunferencia(raio: number): number {
+//         return PI * Math.pow(raio, 2);
+//     }
+//     export function retangulo(base: number, altura: number): number {
+//         return base * altura;
+//     }
+// }
+//importando namespaces
+///<reference path="geometria.ts"/>
+///<reference path="retangulo.ts"/>
 const PI = 25;
-console.log(Areas.circunferencia(10));
-console.log(Areas.retangulo(10, 20));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
+//Namespaces aninhados
+// namespace Geometria {
+//     export namespace Area {
+//         const PI = 3.14;
+//         export function circunferencia(raio: number): number {
+//             return PI * Math.pow(raio, 2);
+//         }
+//         export function retangulo(base: number, altura: number): number {
+//             return base * altura;
+//         }
+//     }
+// }
--- namespaces/namespace.ts
+++ namespaces/namespace.ts
@@ -1,17 +1,39 @@
 //Namespaces são espaços reservados para garantir que nomes iguais de váriaveis globais
 //funções e etc, sejam específicas para aquela classe, e não para os demais arquivos
-namespace Areas {
-    const PI = 3.14;
+// namespace Areas {
+//     const PI = 3.14;
 
-    export function circunferencia(raio: number): number {
-        return PI * Math.pow(raio, 2);
-    }
+//     export function circunferencia(raio: number): number {
+//         return PI * Math.pow(raio, 2);
+//     }
 
-    export function retangulo(base: number, altura: number): number {
-        return base * altura;
-    }
-}
+//     export function retangulo(base: number, altura: number): number {
+//         return base * altura;
+//     }
+// }
+
+//importando namespaces
+///<reference path="geometria.ts"/>
+///<reference path="retangulo.ts"/>
+
 const PI = 25;
-console.log(Areas.circunferencia(10));
-console.log(Areas.retangulo(10, 20));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
+
+//Namespaces aninhados
+// namespace Geometria {
+
+//     export namespace Area {
+//         const PI = 3.14;
+    
+//         export function circunferencia(raio: number): number {
+//             return PI * Math.pow(raio, 2);
+//         }
+    
+//         export function retangulo(base: number, altura: number): number {
+//             return base * altura;
+//         }
+//     }
+
+// }
